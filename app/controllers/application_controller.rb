187b9ed47ae728a546eaa09293a98210a5096b2e
Sixtine Caspar <sixcaspar@gmail.com>
2023-12-21 17:03:12 +0100

    add regaistration with rails 7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def login(user)
+    Current.user = user
+    reset_session
+    session[:user_id] = user.id
+  end
+
+  def logout
+    Current.user = nil
+    reset_session
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,19 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  root "home#index"
+
+  resource :session
+  resource :registration
+  resource :password_reset
+  resource :password
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  if Rails.env.local?
+    namespace :biliotherapy do
+      root "home#index"
+    end
+  end
 end
